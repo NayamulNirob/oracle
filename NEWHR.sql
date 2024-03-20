dc2fba4f56eb22be87b195cbe64bdccbb53c507b
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-03-20 16:32:38 +0600

    a
    dd

---
+++ NEWHR.sql
@@ -0,0 +1,9 @@
+select * from employees
+where salary BETWEEN 20000 AND 30000;
+
+select * from employees
+where first_name like '%k%';
+
+
+select * from employees
+where salary in (3600,8400,8200);
