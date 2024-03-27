584e01f94cdc3ce8fa81dc717b3f91dd19a556cb
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-03-27 16:25:09 +0600

    27+03+2024
    Add

---
+++ NEWHR class 9.sql
@@ -0,0 +1,19 @@
+select last_name,salary,commission_pct,
+coalesce((salary+(commission_pct*salary)),salary+2000)"New salary"
+from employees;
+
+
+select last_name,salary,commission_pct,NVL(commission_pct,0),(salary*12)+
+(salary *12*NVL(commission_pct,0))AN_SAl
+from employees;
+
+select to_date('May 24,2007','fxMonth DD,YYYY')
+from dual;
+
+select last_name,salary,commission_pct,NVL(commission_pct,1)
+from employees;
+
+select last_name,salary,commission_pct,
+NVL2(commission_pct, salary + (salary * commission_pct), salary)as "New salary"
+from employees;
+
