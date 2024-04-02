df6962bcbf96082a27ad17b4c5e401a3896ba6c6
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-04-02 16:32:57 +0600

    A
    aa

--- NEWHR evidence .sql
+++ NEWHR evidence .sql
@@ -3,8 +3,6 @@ SELECT first_name,last_name,hire_date,salary
 FROM employees
 where salary>10000;
 
-
-
 SELECT first_name,hire_date,salary
 FROM employees
 where job_id='IT_PROG' OR job_id = 'SA_MAN';
@@ -14,6 +12,7 @@ FROM employees
 where  employee_id=150 OR employee_id=160;
 
 
-SELECT job_title,min_salary,max_salary,max_salary-min_salary
+SELECT job_title,max_salary,min_salary,max_salary-min_salary
     FROM jobs
     Where Max_salary between 10000 and 20000; 
+    
